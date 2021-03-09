ed63a2c653c4db56e1d7df08f4ead19240f28ee8
hikdul <hikdul.lio@gmail.com>
2021-03-09 10:11:23 -0500

    agregando detalles y generando prueba

---
+++ MyLoginWhitJWT/Controllers/API/ValuesController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyLoginWhitJWT.Controllers.API
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+    public class ValuesController : ControllerBase
+    {
+
+
+        [HttpGet]
+        public string GET()
+        {
+            return "Esta es la clave secreta";
+        }
+
+        [HttpGet("id")]
+        public string GETById(int id)
+        {
+                
+            return "El Valor secreto es: " + id;
+
+        }
+
+    }
+}
--- MyLoginWhitJWT/Startup.cs
+++ MyLoginWhitJWT/Startup.cs
@@ -70,6 +70,7 @@ namespace MyLoginWhitJWT
 
             app.UseRouting();
 
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
