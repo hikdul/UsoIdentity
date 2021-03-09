b7963173f54d74a7de55e250a05df0368ed8351f
hikdul <hikdul.lio@gmail.com>
2021-03-09 13:19:24 -0500

    cuentas controller Y antes de hacer prueba cion el identity de scanfolding

---
+++ MyLoginWhitJWT/Controllers/AccountsController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyLoginWhitJWT.Controllers
+{
+    /// <summary>
+    /// en este controlador iran todos los elementos que se solicitan para poder tener acceso a las cuentas de usuario como loggearse y demas
+    /// </summary>
+    public class AccountsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
